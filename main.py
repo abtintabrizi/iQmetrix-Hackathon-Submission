6bd6e7b295f124dbcc373454a9b198c621d049bf
STr3AK_ <tabrizi2002@yahoo.com>
2021-04-05 23:51:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font as tkfont
+from tkinter import filedialog, asksaveasfile
 
 
 class SampleApp(tk.Tk):
@@ -15,7 +16,7 @@ class SampleApp(tk.Tk):
             family='Helvetica', size=18, weight="bold")
 
         self.frames = {}
-        for F in (StartPage, EmployerPage, EmployeePage):
+        for F in (StartPage, EmployerPage, EmployeePage, ViewFilesPage, AddFilePage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -65,6 +66,66 @@ class EmployeePage(tk.Frame):
         button = tk.Button(self, text="Log Out", bg="royal blue", borderwidth=0,
                            command=lambda: controller.show_frame("StartPage"))
         button.place(relheight=0.1, relwidth=0.1)
+        title = tk.Label(self, text="Profile",
+                         font=controller.title_font, bg="light blue")
+        title.place(relx=0.5, rely=0.05)
+        offerHistoryButton = tk.Button(self, text="Offer History", bg="royal blue", borderwidth=0,
+                                       command=lambda: controller.show_frame("StartPage"))
+        offerHistoryButton.place(
+            relwidth=0.25, relheight=0.33, relx=0.1, rely=0.33)
+        skillsProfileButton = tk.Button(self, text="Skills Profile", bg="royal blue", borderwidth=0,
+                                        command=lambda: controller.show_frame("StartPage"))
+        skillsProfileButton.place(
+            relwidth=0.25, relheight=0.33, relx=0.4, rely=0.33)
+        viewFilesButton = tk.Button(self, text="View Files", bg="royal blue", borderwidth=0,
+                                    command=lambda: controller.show_frame("ViewFilesPage"))
+        viewFilesButton.place(
+            relwidth=0.25, relheight=0.33, relx=0.7, rely=0.33)
+
+
+class ViewFilesPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.configure(bg="light blue")
+        button = tk.Button(self, text="Back", bg="royal blue", borderwidth=0,
+                           command=lambda: controller.show_frame("EmployeePage"))
+        button.place(relheight=0.1, relwidth=0.1)
+        title = tk.Label(self, text="View Files",
+                         font=controller.title_font, bg="light blue")
+        title.place(relx=0.5, rely=0.05)
+        addFileButton = tk.Button(self, text="Add File", bg="royal blue", borderwidth=0,
+                                  command=lambda: controller.show_frame("AddFilePage"))
+        addFileButton.place(relx=0.45, rely=0.8, relwidth=0.2, relheight=0.1)
+
+
+class AddFilePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.configure(bg="light blue")
+        button = tk.Button(self, text="Back", bg="royal blue", borderwidth=0,
+                           command=lambda: controller.show_frame("ViewFilesPage")).place(relheight=0.1, relwidth=0.1)
+        title = tk.Label(self, text="Add File", font=controller.title_font,
+                         bg="light blue").place(relx=0.5, rely=0.05)
+        tk.Label(self, text="Upload", font=controller.title_font,
+                 bg="light blue").place(relx=0.35, rely=0.35)
+        uploadButton = tk.Button(self, text="Upload", bg="royal blue", borderwidth=0,
+                                 command=self.get).place(relx=0.45, rely=0.8, relwidth=0.2, relheight=0.1)
+        attachButton = tk.Button(self, text="Attach", bg="royal blue", borderwidth=0,
+                                 command=self.attachAction).place(relx=0.45, rely=0.5, relwidth=0.2, relheight=0.1)
+
+        self.name = tk.Entry(self)
+        self.name.place(relx=0.5, rely=0.35)
+
+    def get(self):
+        print(self.name.get())
+
+    def attachAction(event=None):
+        filename = filedialog.askopenfilename(initialdir="/", title="Select a File", filetypes=(("Text files", "*.txt*"), ("all files","*.*")))
+        print(filename)
 
 
 if __name__ == "__main__":
